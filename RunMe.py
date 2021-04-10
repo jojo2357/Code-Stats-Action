9444229722d887bdc8d86de89f9ef7a15546793f
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2021-04-10 13:02:12 -0700

    Trying the do and removing idea

--- RunMe.py
+++ RunMe.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pprint
+import sys
 from os.path import join, isfile
 from typing import Dict
 
@@ -58,24 +59,26 @@ if __name__ == '__main__':
                         style="{")
     logger = logging.getLogger()
     logger.setLevel(10)
+
+    logger.debug(sys.argv)
     # Get a list of all the java files
     ALL_FILES = find_all_java_files()
     ALL_STATS: Dict[str, int] = {"total": 0, "blanks": 0, "comments": 0, "code": 0}
     logger.debug(pprint.pformat(ALL_STATS))
     # Now write the text file
+    ALL_DATA = []
+    for fileName in ALL_FILES:
+        ALL_DATA.append({"name": fileName, "goods": read_and_get_the_goods(fileName)})
+    for index, goods in enumerate(ALL_DATA, start=1):
+        ALL_STATS["total"] += goods["goods"]["total"]
+        ALL_STATS["code"] += goods["goods"]["code"]
+        ALL_STATS["comments"] += goods["goods"]["comments"]
+        ALL_STATS["blanks"] += goods["goods"]["blanks"]
+        print(str(goods["name"]).split("\\")[len(str(goods["name"]).split("\\")) - 1])
     with open("Statistic.csv", "w") as file:
         logger.info("Created the text file")
         file.write(
-            "File,Lines (% total),Code Lines (% total),Comment Lines (% total),Blank Lines (% total),% Code,% Comment,% Blank")
-        ALL_DATA = []
-        for fileName in ALL_FILES:
-            ALL_DATA.append({"name": fileName, "goods": read_and_get_the_goods(fileName)})
-        for index, goods in enumerate(ALL_DATA, start=1):
-            ALL_STATS["total"] += goods["goods"]["total"]
-            ALL_STATS["code"] += goods["goods"]["code"]
-            ALL_STATS["comments"] += goods["goods"]["comments"]
-            ALL_STATS["blanks"] += goods["goods"]["blanks"]
-            print(str(goods["name"]).split("\\")[len(str(goods["name"]).split("\\")) - 1])
+            "File,Lines (% total),Code Lines (% total),% Code,Comment Lines (% total),% Comment,Blank Lines (% total),% Blank")
         for index, goods in enumerate(ALL_DATA, start=1):
             file.write("\n" + str(goods["name"]).split("\\")[len(str(goods["name"]).split("\\")) - 1] +
                        "," + str(goods["goods"]["total"]) + " (" + str(
@@ -95,8 +98,31 @@ if __name__ == '__main__':
                    "," + str(ALL_STATS["code"]) + "," +
                    "," + str(ALL_STATS["comments"]) + "," +
                    "," + str(ALL_STATS["blanks"]) + ",")
-        print("\nTotal" +
-              "," + str(ALL_STATS["total"]) +
-              "," + str(ALL_STATS["code"]) + "," +
-              "," + str(ALL_STATS["comments"]) + "," +
-              "," + str(ALL_STATS["blanks"]) + ",")
+    with open("Statistic.md", "w") as file:
+        file.write(
+            "\n|File|Lines (% total)|Code Lines (% total)|% Code|Comment Lines (% total)|% Comment|Blank Lines (% total)|% Blank|")
+        file.write("\n| --- | --- | --- | --- | --- | --- | --- | --- |")
+        for index, goods in enumerate(ALL_DATA, start=1):
+            file.write("\n|" + str(goods["name"]).split("\\")[len(str(goods["name"]).split("\\")) - 1] +
+                       "|" + str(goods["goods"]["total"]) + " (" + str(
+                format(100 * goods["goods"]["total"] / ALL_STATS["total"], ".1f")) + "%)" +
+                       "|" + str(goods["goods"]["code"]) + " (" + str(
+                format(100 * goods["goods"]["code"] / ALL_STATS["code"], ".1f")) + "%)" + "|" +
+                       str(format(100 * goods["goods"]["code"] / goods["goods"]["total"], ".1f")) + "%" +
+                       "|" + str(goods["goods"]["comments"]) + " (" + str(
+                format(100 * goods["goods"]["comments"] / ALL_STATS["comments"], ".1f")) + "%)" + "|" +
+                       str(format(100 * goods["goods"]["comments"] / goods["goods"]["total"], ".1f")) + "%" +
+                       "|" + str(goods["goods"]["blanks"]) + " (" + str(
+                format(100 * goods["goods"]["blanks"] / ALL_STATS["blanks"], ".1f")) + "%)" + "|" +
+                       str(format(100 * goods["goods"]["blanks"] / goods["goods"]["total"], ".1f")) + "%|")
+        file.write("\n|Total" +
+                   "|" + str(ALL_STATS["total"]) +
+                   "|" + str(ALL_STATS["code"]) + "| " +
+                   "|" + str(ALL_STATS["comments"]) + "| " +
+                   "|" + str(ALL_STATS["blanks"]) + "| |")
+
+    print("\nTotal" +
+          "," + str(ALL_STATS["total"]) +
+          "," + str(ALL_STATS["code"]) + "," +
+          "," + str(ALL_STATS["comments"]) + "," +
+          "," + str(ALL_STATS["blanks"]) + ",")
